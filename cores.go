2348e082752b8b9d7f147976e43798cc1b488e24
Bob <b.e.droge@rug.nl>
2019-03-27 14:29:24 +0100

    Remove unused import

--- cores.go
+++ cores.go
@@ -16,7 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 package main
 
 import (
-        "fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"io/ioutil"
 	"log"
