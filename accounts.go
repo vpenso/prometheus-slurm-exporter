d782cc27f0a0d964bde7eb907eb70107a604ee5b
Victor Penso <vic.penso@gmail.com>
2020-10-08 13:07:41 +0200

    add some more states

--- accounts.go
+++ accounts.go
@@ -41,8 +41,11 @@ func AccountsData() []byte {
 }
 
 type JobMetrics struct {
+        cancelled float64
+        completed float64
         pending float64
         running float64
+        suspended float64
 }
 
 func ParseAccountsMetrics(input []byte) map[string]*JobMetrics {
@@ -53,17 +56,26 @@ func ParseAccountsMetrics(input []byte) map[string]*JobMetrics {
                         account := strings.Split(line,"|")[1]
                         _,key := accounts[account]
                         if !key {
-                                accounts[account] = &JobMetrics{0,0}
+                                accounts[account] = &JobMetrics{0,0,0,0,0}
                         }
                         state := strings.Split(line,"|")[2]
                         state = strings.ToLower(state)
-                        running := regexp.MustCompile(`^running`)
+                        cancelled := regexp.MustCompile(`^cancelled`)
+                        completed := regexp.MustCompile(`^completed`)
                         pending := regexp.MustCompile(`^pending`)
+                        running := regexp.MustCompile(`^running`)
+                        suspended := regexp.MustCompile(`^suspended`)
                         switch {
-                        case running.MatchString(state) == true:
-                                accounts[account].running++
+                        case cancelled.MatchString(state) == true:
+                                accounts[account].cancelled++
+                        case completed.MatchString(state) == true:
+                                accounts[account].completed++
                         case pending.MatchString(state) == true:
                                 accounts[account].pending++
+                        case running.MatchString(state) == true:
+                                accounts[account].running++
+                        case suspended.MatchString(state) == true:
+                                accounts[account].suspended++
                         }
                 }
         }
@@ -71,28 +83,39 @@ func ParseAccountsMetrics(input []byte) map[string]*JobMetrics {
 }
 
 type AccountsCollector struct {
-        running *prometheus.Desc
+        cancelled *prometheus.Desc
+        completed *prometheus.Desc
         pending *prometheus.Desc
+        running *prometheus.Desc
+        suspended *prometheus.Desc
 }
 
 func NewAccountsCollector() *AccountsCollector {
         labels := []string{"account"}
         return &AccountsCollector{
+                cancelled: prometheus.NewDesc("slurm_account_jobs_cancelled", "Cancelled jobs for account", labels, nil),
+                completed: prometheus.NewDesc("slurm_account_jobs_completed", "Completed jobs for account", labels, nil),
                 running: prometheus.NewDesc("slurm_account_jobs_running", "Running jobs for account", labels, nil),
                 pending: prometheus.NewDesc("slurm_account_jobs_pending", "Pending jobs for account", labels, nil),
+                suspended: prometheus.NewDesc("slurm_account_jobs_suspended", "Suspended jobs for account", labels, nil),
         }
 }
 
 func (ac *AccountsCollector) Describe(ch chan<- *prometheus.Desc) {
-        ch <- ac.running
+        ch <- ac.cancelled
+        ch <- ac.completed
         ch <- ac.pending
+        ch <- ac.running
+        ch <- ac.suspended
 }
 
 func (ac *AccountsCollector) Collect(ch chan<- prometheus.Metric) {
         am := ParseAccountsMetrics(AccountsData())
         for a := range am {
-                log.Print(a)
-                ch <- prometheus.MustNewConstMetric(ac.running, prometheus.GaugeValue, am[a].running, a)
+                ch <- prometheus.MustNewConstMetric(ac.cancelled, prometheus.GaugeValue, am[a].cancelled, a)
+                ch <- prometheus.MustNewConstMetric(ac.completed, prometheus.GaugeValue, am[a].completed, a)
                 ch <- prometheus.MustNewConstMetric(ac.pending, prometheus.GaugeValue, am[a].pending, a)
+                ch <- prometheus.MustNewConstMetric(ac.running, prometheus.GaugeValue, am[a].running, a)
+                ch <- prometheus.MustNewConstMetric(ac.suspended, prometheus.GaugeValue, am[a].suspended, a)
         }
 }
