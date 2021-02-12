8420a24feffe5225cb492998ea22d5c64de70579
Rovanion Luckey <rovanion.luckey@gmail.com>
2021-02-12 11:12:53 +0100

    Corrected function name in cpus_test.go

--- cpus_test.go
+++ cpus_test.go
@@ -1,4 +1,4 @@
-/* Copyright 2017 Victor Penso, Matteo Dessalvi
+/* Copyright 2017 Victor Penso, Matteo Dessalvi, Rovanion Luckey
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -31,6 +31,6 @@ func TestCPUsMetrics(t *testing.T) {
 	t.Logf("%+v", ParseCPUsMetrics(data))
 }
 
-func TestCPUssGetMetrics(t *testing.T) {
+func TestCPUsGetMetrics(t *testing.T) {
 	t.Logf("%+v", CPUsGetMetrics())
 }
